0d3fd3709c53810ecbf77a4a71ebd45cb2eb0456
Nick Wilson <WilsonN11@cardiff.ac.uk>
2022-09-26 14:51:37 +0100

    initial script for conda-forge

---
+++ conda-forge.sh
@@ -0,0 +1,40 @@
+# This script is used for building for conda-forge
+#
+
+# github releases are not under version control so pass correct version as argument to script
+version=${1}
+
+PREFIX=${PREFIX:-${CONDA_PREFIX}}
+BUILD=cmake-build
+echo PREFIX=${PREFIX}
+echo CONDA_PREFIX=${CONDA_PREFIX}
+
+if [ -x "$(command -v $CONDA_PREFIX/bin/cc)" ]
+then
+    CC=$CONDA_PREFIX/bin/cc
+fi
+
+if [ -x "$(command -v $CONDA_PREFIX/bin/cpp)" ]
+then
+    CXX=$CONDA_PREFIX/bin/c++
+fi
+
+echo '__version__ = "'${version}'"' > pysjef/_version.py
+
+mkdir -p $BUILD
+cd $BUILD
+if [ -f "install_manifest.txt" ]
+then
+    xargs rm < "install_manifest.txt"
+fi
+if [ -f "CMakeCache.txt" ]
+then
+    rm CMakeCache.txt
+fi
+cmake ../.. -DCMAKE_INSTALL_PREFIX=$PREFIX -DBUILD_TESTS=OFF -DBUILD_PROGRAM=OFF || { echo 'cmake build failed' ; exit 1; }
+cmake --build . -t install || { echo 'make install failed' ; exit 1; }
+cd ../
+
+#PREFIX=$PREFIX python setup.py build_ext --inplace
+#PREFIX=$PREFIX python -m pip install --no-deps --force-reinstall --verbose . 
+PREFIX=$PREFIX python -m pip install -e . 
